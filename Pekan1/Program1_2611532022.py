d8397a319a1e0df288bed30379ea4f56e6c44fa6
PhytonAziii <malikaazizahmuskhir2@gmail.com>
2026-09-03 10:35:20 +0800

    praktikum pekan 1

---
+++ Pekan1/Program1_2611532022.py
@@ -0,0 +1,11 @@
+#program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("program ini menghasilkan")
+print(" / \\ // \\\\ /// \\\\\ ")
+print("program ini" + "dibuat oleh a\n" + "Malika Azizah Muskhir 2611532022")
