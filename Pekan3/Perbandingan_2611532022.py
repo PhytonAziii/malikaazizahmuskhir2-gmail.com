d095970ac2fad6128d621d37cebc0fec8dac7ca0
PhytonAziii <malikaazizahmuskhir2@gmail.com>
2026-09-18 18:40:58 +0800

    Update Perbandingan_2611532022.py

--- Pekan3/Perbandingan_2611532022.py
+++ Pekan3/Perbandingan_2611532022.py
@@ -10,37 +10,37 @@ angka2_2022 = int(input("Input angka-2: "))
 # Lebih besar dari
 hasil_2022 = angka1_2022 > angka2_2022
 print("\nOperator lebih besar dari")
-print("angka1 > angka2 =",hasil_2022)
+print("angka1_2022 > angka2_2022 =",hasil_2022)
 
 # Lebih kecil dari
 hasil_2022 = angka1_2022 < angka2_2022
 print("\nOperator lebih kecil dari")
-print("angka1 < angka2 =",hasil_2022)
+print("angka1_2022 < angka2_2022 =",hasil_2022)
 
 # Lebih besar dari atau sama dengan
 hasil_2022 = angka1_2022 >= angka2_2022
 print("\nOperator lebih besar dari atau sama dengan")
-print("angka1 >= angka2 =",hasil_2022)
+print("angka1_2022 >= angka2_2022 =",hasil_2022)
 
 # Lebih kecil dari atau sama dengan
 hasil_2022 = angka1_2022 <= angka2_2022
 print("\nOperator lebih kecil dari atau sama dengan")
-print("angka1 <= angka2 =",hasil_2022)
+print("angka1_2022 <= angka2_2022 =",hasil_2022)
 
 # Sama dengan
 hasil_2022 = angka1_2022 == angka2_2022
 print("\nOperator sama dengan")
-print("angka1 == angka2 =",hasil_2022)
+print("angka1_2022 == angka2_2022 =",hasil_2022)
 
 # Tidak sama dengan
 hasil_2022 = angka1_2022 != angka2_2022
 print("\nOperator tidak sama dengan")
-print("angka1 != angka2 =",hasil_2022)
+print("angka1_2022 != angka2_2022 =",hasil_2022)
 
 # Tambahan: perbandingan berantai dalam Python
 hasil_2022 = 0 < angka1_2022 < 100
 print("\nPerbandingan berantai")
-print("0 < angka1_1021 < 100 =",hasil_2022)
+print("0 < angka1_1022 < 100 =",hasil_2022)
 
 hasil_2022 = 0 < angka2_2022 < 100
-print("0 < angka2_1021 < 100 =",hasil_2022)
+print("0 < angka2_1022 < 100 =",hasil_2022)
